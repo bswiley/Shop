c4745cb5cf6d6499d188ea6c3f7a2c07aa7f622c
bswiley <bswiley@gmail.com>
2023-06-04 16:54:40 -0500

    fixed seeding problem

--- models/index.js
+++ models/index.js
@@ -18,6 +18,7 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
+  foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
